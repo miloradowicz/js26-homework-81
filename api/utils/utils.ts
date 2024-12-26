7661fff5ba81766962f3d1ee841853f3dc9e6723
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-12-26 10:10:27 +0600

    short url generator created

---
+++ api/utils/utils.ts
@@ -0,0 +1,9 @@
+const chars = 'AB'; // 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+export const getRandomString = (length: number) => {
+  if (Number.isNaN(length) || length < 1) {
+    throw new Error('Length must be a positive integer.');
+  }
+
+  return Array.from({ length }, () => chars.charAt(Math.floor(Math.random() * chars.length))).join('');
+};
